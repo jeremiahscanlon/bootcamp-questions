16079176df551488ac6d05bc703922834d1fa5a0
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-04-06 01:33:05 -0400

    added question and firebase

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function(){
+
+	console.log('home.js loaded.');
+
+	// mobile menu
 	$(".button-collapse").sideNav();
-	console.log('yerp');
+
+
 });
---
+++ assets/js/question.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+	
+	console.log('question.js loaded.');
+
+	// Materialize character counter
+	$('input#input_text, textarea#textarea1').characterCounter();
+
+});
